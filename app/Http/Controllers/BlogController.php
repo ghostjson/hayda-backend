8b059636118d1d7bec4965d50fe21987724da878
Akhil Ravindran <akhilravindran3@gmail.com>
2020-11-13 22:01:29 +0530

    added blog CRUD

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BlogCreateRequest;
+use App\Http\Requests\BlogUpdateRequest;
+use App\Http\Resources\BlogResource;
+use App\Models\Blog;
+use Illuminate\Support\Facades\Log;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        return BlogResource::collection(Blog::all());
+    }
+
+    public function create(BlogCreateRequest $request)
+    {
+        try {
+            $blog = Blog::create($request->validated());
+            return respondWithObject('Successfully updated', $blog, 200);
+        }catch(\Exception $exception){
+            Log::error($exception);
+            return respond('Server Error', 500);
+        }
+    }
+
+    public function show(Blog $blog)
+    {
+        return new BlogResource($blog);
+    }
+
+    public function destroy(Blog $blog)
+    {
+        try {
+            $blog->delete();
+            return respond('Successfully deleted', 200);
+        }catch(\Exception $exception){
+            Log::error($exception);
+            return respond('Server Error', 500);
+        }
+    }
+
+    public function update(Blog $blog, BlogUpdateRequest $request)
+    {
+        try {
+            $blog->update($request->validated());
+            return respondWithObject('Successfully updated', $blog);
+        }catch(\Exception $exception){
+            Log::error($exception);
+            return respond('Server Error', 500);
+        }
+    }
+}
--- routes/api/blog_routes.php
+++ routes/api/blog_routes.php
@@ -1,1 +1,10 @@
 <?php
+
+use App\Http\Controllers\BlogController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('', [BlogController::class, 'index']);
+Route::post('', [BlogController::class, 'create']);
+Route::get('{blog}', [BlogController::class, 'show']);
+Route::delete('{blog}', [BlogController::class, 'destroy']);
+Route::post('{blog}', [BlogController::class, 'update']);
