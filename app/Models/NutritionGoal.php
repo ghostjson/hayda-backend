ca768a8842009faa6017508b0749c8ab6f646d1c
Akhil Ravindran <akhilravindran3@gmail.com>
2021-01-21 13:56:17 +0530

    fix date format of nutrition page

--- app/Models/NutritionGoal.php
+++ app/Models/NutritionGoal.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +14,9 @@ class NutritionGoal extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return date('d-m-Y', strtotime($value));
+    }
 }
