b8ad034d59cdd2c8cf79f45addcb64ba15a2fa1c
Akhil Ravindran <akhilravindran3@gmail.com>
2021-02-04 21:42:09 +0530

    chatbot implemented

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ChatRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ChatController extends Controller
 {
     public function run(ChatRequest $request)
     {
         $message = $request->input('message');
+
+        $response = Http::asForm()->post('http://127.0.0.1:5000/reply', [
+            'message' => $message
+        ]);
+
+
         return [
-            'message' => 'Hello there, what can I do for you?'
+            'message' => $response->body()
         ];
     }
 
